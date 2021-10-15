a5fa0ef15000dc9a8029114cddcd1b4560642676
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2021-10-15 18:12:21 +0530

    Time: 2 ms (20.16%), Space: 7.1 MB (75.09%) - LeetHub

---
+++ max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n=arr.size();
+        int cnt=0;
+        int maxi=-1;
+        for(int i=0;i<n;i++)
+        {
+            maxi=max(maxi,arr[i]);
+            if(maxi==i)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
