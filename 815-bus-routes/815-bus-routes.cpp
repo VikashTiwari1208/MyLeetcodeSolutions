05d5c71a68e4e90697ad27ad7f267c7e0b78ae5b
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-07 18:40:40 +0530

    Time: 553 ms (14.39%), Space: 66.2 MB (32.34%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
    
-    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-      int n = routes.size();
-        unordered_map<int, unordered_set<int>> mp;
-        
-        for(int i=0; i<n; i++){
-            for(int& j: routes[i]) mp[j].insert(i);
+    int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
+        if(S==T)
+        {
+            return 0;
         }
-        
-        if(mp.find(source) == mp.end() || mp.find(target) == mp.end()) return -1;
-        if(target == source) return 0;
-        
-        vector<vector<int>> adj(501);
-        for(int u=0; u<n; u++){
-            for(int& stop: routes[u]){
-                for(auto& v: mp[stop]){
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                }
-            }
-        }
-        
-        queue<int> q;
-        vector<bool> visited(501, false);
-        for(auto& start: mp[source]){
-            q.push(start);
-            visited[start] = true;
-        }
-        int minCount = 1;
-        
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                int u = q.front();
-                q.pop();
-                //cout << u << endl;
-                if(mp[target].find(u) != mp[target].end()) return minCount;
-                for(int& v: adj[u]){
-                    if(visited[v] != true){
-                        visited[v] = true;
-                        q.push(v);
+          unordered_map<int, vector<int>> to_routes;
+        for (int i = 0; i < routes.size(); ++i)
+            for (int j : routes[i])
+                to_routes[j].push_back(i);
+        queue<pair<int, int>> bfs;
+        bfs.push({S, 0});
+        set<int> seen = {S};
+        while (!bfs.empty()) {
+            int stop = bfs.front().first, bus = bfs.front().second;
+            bfs.pop();
+            if (stop == T)
+                return bus;
+            for (int i : to_routes[stop]) {
+                for (int j : routes[i]) {
+                    if (seen.find(j) == seen.end()) {
+                        seen.insert(j);
+                        bfs.push({j, bus + 1});
                     }
                 }
+                routes[i].clear();
             }
-            minCount ++;
         }
-        
         return -1;
     }
 };
