736692a33bf5fcfc05fb8d6c08083ad60886e543
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-07 18:39:22 +0530

    Time: 236 ms (56.83%), Space: 123.5 MB (9.14%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -1,77 +1,52 @@
 class Solution {
 public:
-    map<int,set<int>>mp;
-    bool helper(vector<int>&a,vector<int>&b,set<int>&s1)
-    {
-      
-      // mp[i]=s1;
-        for(auto x:b)
-        {
-            if(s1.find(x)!=s1.end())
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+   
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        if(source==target)
-        {
-            return 0;
-        }
-        int n=routes.size();
-        map<int,vector<int>>adj;
+      int n = routes.size();
+        unordered_map<int, unordered_set<int>> mp;
         
-        for(int i=0;i<n;i++)
-        {
-            set<int>s;
-            for(auto x:routes[i])
-            {
-                s.insert(x);
-            }
-            mp[i]=s;
-            for(int j=i+1;j<n;j++)
-            {
-                if(helper(routes[i],routes[j],mp[i]))
-                {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        for(int i=0; i<n; i++){
+            for(int& j: routes[i]) mp[j].insert(i);
+        }
+        
+        if(mp.find(source) == mp.end() || mp.find(target) == mp.end()) return -1;
+        if(target == source) return 0;
+        
+        vector<vector<int>> adj(501);
+        for(int u=0; u<n; u++){
+            for(int& stop: routes[u]){
+                for(auto& v: mp[stop]){
+                    adj[u].push_back(v);
+                    adj[v].push_back(u);
                 }
             }
         }
-         
-        queue<pair<int,int>>q;
-        // bus,dis;
-        for(int i=0;i<n;i++)
-        {
-            if(mp[i].find(source)!=mp[i].end())
-            {
-                q.push({i,0});
-            }
+        
+        queue<int> q;
+        vector<bool> visited(501, false);
+        for(auto& start: mp[source]){
+            q.push(start);
+            visited[start] = true;
         }
-       
-        set<int>vis;
-        while(q.empty()==false)
-        {
-            auto x=q.front();
-            q.pop();
-            int bus=x.first;
-            int dis=x.second;
-           // cout<<bus<<endl;
-            vis.insert(bus);
-            if(mp[bus].find(target)!=mp[bus].end())
-            {
-                return dis+1;
-            }
-            for(auto el:adj[bus])
-            {
-                //cout<<el<<" ";
-                if(vis.find(el)==vis.end())
-                {
-                    q.push({el,dis+1});
+        int minCount = 1;
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int u = q.front();
+                q.pop();
+                //cout << u << endl;
+                if(mp[target].find(u) != mp[target].end()) return minCount;
+                for(int& v: adj[u]){
+                    if(visited[v] != true){
+                        visited[v] = true;
+                        q.push(v);
+                    }
                 }
             }
+            minCount ++;
         }
+        
         return -1;
     }
 };
