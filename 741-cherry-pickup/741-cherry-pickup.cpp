968a27c8775a134772e1e41190991cdd0777132d
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-11 17:22:44 +0530

    Time: 88 ms (36.33%), Space: 9.5 MB (92.30%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
-    int dp[51][51][51];
-    int n,m;
-    int helper(int r1,int c1,int r2,vector<vector<int>>&grid)
-    {
-        int c2=r1+c1-r2;
-        if(r1>=n||c1>=m||r2>=n||c2>=m||grid[r1][c1]==-1||grid[r2][c2]==-1)
-        {
-            return INT_MIN;
-        }
-        if(r1==n-1&&c1==m-1)
-        {
-            return grid[n-1][m-1];
-        }
-        if(dp[r1][c1][r2]!=0)
-        {
-            return dp[r1][c1][r2];
-        }
-        int chr=0;
-        if(r1==r2&&c1==c2)
-        {
-            chr+=grid[r1][c1];
-        }
-        else
-        {
-            chr+=grid[r1][c1]+grid[r2][c2];
-        }
-        // hh vv vh hv
-        int op1=helper(r1,c1+1,r2,grid);
-        int op2=helper(r1+1,c1,r2+1,grid);
-        int op3=helper(r1+1,c1,r2,grid);
-        int op4=helper(r1,c1+1,r2+1,grid);
-        chr+=max({op1,op2,op3,op4});
-        return dp[r1][c1][r2]=chr;
-    }
+    
     int cherryPickup(vector<vector<int>>& grid) {
-       memset(dp,0,sizeof(dp));
-        n=grid.size();
-        m=grid[0].size();
-        int ans=helper(0,0,0,grid);
-        if(ans<0)
+       int n=grid.size();
+        int dp[n+1][n+1][n+1];
+        for(int i=0;i<=n;i++)
         {
-            return 0;
+            for(int j=0;j<=n;j++)
+            {
+                for(int k=0;k<=n;k++)
+                {
+                    dp[i][j][k]=INT_MIN;
+                }
+            }
         }
-        return ans;
+        dp[1][1][1]=grid[0][0];
+        for(int r1=1;r1<=n;r1++)
+        {
+            for(int c1=1;c1<=n;c1++)
+            {
+                for(int r2=1;r2<=n;r2++)
+                {
+                    int c2=r1+c1-r2;
+                    if(c2>n||c2<1||dp[r1][c1][r2]>0||grid[r1-1][c1-1]==-1||grid[r2-1][c2-1]==-1)
+                    {
+                        continue;
+                    }
+                int temp=max({dp[r1][c1-1][r2],dp[r1-1][c1][r2-1],dp[r1][c1-1][r2-1],dp[r1-1][c1][r2]});
+                    if(temp<0)
+                    {
+                        continue;
+                    }
+                    int curr=0;
+                    if(r1!=r2)
+                    {
+                        curr+=grid[r1-1][c1-1]+grid[r2-1][c2-1];
+                    }
+                    else
+                    {
+                        curr+=grid[r1-1][c1-1];
+                    }
+                    dp[r1][c1][r2]=curr+temp;
+                }
+            }
+        }
+        if(dp[n][n][n]<0)
+        {
+            return  0;
+        }
+        return dp[n][n][n];
     }
 };
