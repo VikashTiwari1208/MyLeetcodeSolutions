e36c48bb0293033807a24ef75bdef14693b51c82
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-12 14:04:02 +0530

    Time: 4 ms (78.41%), Space: 8 MB (92.06%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>>dp;
-    int helper(int arr_idx,int dish_idx,vector<int>&arr)
-    {
-        if(arr_idx>=n)
-        {
-            return 0;
-        }
-        if(dp[arr_idx][dish_idx]!=-1)
-        {
-            return dp[arr_idx][dish_idx];
-        }
-        int ans1=arr[arr_idx]*(dish_idx+1)+helper(arr_idx+1,dish_idx+1,arr);//lelia
-        int ans2=helper(arr_idx+1,dish_idx,arr);//nhi lia
-            return dp[arr_idx][dish_idx]=max(ans1,ans2);
-    }
+    
     int maxSatisfaction(vector<int>& arr) {
         sort(arr.begin(),arr.end());
-         n=arr.size();
+        int n=arr.size();
         if(arr[n-1]<=0)
         {
             return 0;
         }
-        dp.resize(n,vector<int>(n,-1));
-        return helper(0,0,arr);
-        
+        int ans=0;
+        int curr_sum=0;
+        int suff_sum=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            suff_sum+=arr[i];
+            curr_sum+=suff_sum;
+            ans=max(ans,curr_sum);
+            
+        }
+        return ans;
     }
 };
