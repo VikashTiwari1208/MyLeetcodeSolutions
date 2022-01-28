e4f3723504e1e4a30082ceefbd10e090d699bef5
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-28 23:32:02 +0530

    Time: 974 ms (8.19%), Space: 106.6 MB (7.94%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
-    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        int n=students[0].size();
-        int m=students.size();
-        vector<int>v;
-        for(int i=0;i<m;i++)
+    int dp[10][256];
+    int score(vector<int>s,vector<int>&m)
+    {
+        int ans=0;
+        int n=s.size();
+        for(int i=0;i<n;i++)
         {
-            v.push_back(i);
+            if(s[i]==m[i])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+    int helper(int i,int mask,vector<vector<int>>&s,vector<vector<int>>&m)
+    {
+        int n=s.size();
+        if(i==n)
+        {
+            return 0;
+        }
+        if(dp[i][mask]!=-1)
+        {
+            return dp[i][mask];
         }
         int ans=0;
-        do{
-            
-            int curr_sum=0;
-            for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+        {
+            if((mask&(1<<j))==0)
             {
-                int temp=0;
-                for(int j=0;j<n;j++)
-                {
-                    if(students[v[i]][j]==mentors[i][j])
-                    {
-                        temp++;
-                    }
-                }
-                curr_sum+=temp;
+                int n_mask=mask|(1<<j);
+                ans=max(ans,score(s[i],m[j])+helper(i+1,n_mask,s,m));
             }
-            ans=max(ans,curr_sum);
-        }while(next_permutation(v.begin(),v.end()));
+        }
         return ans;
+    }
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,students,mentors);
     }
 };
