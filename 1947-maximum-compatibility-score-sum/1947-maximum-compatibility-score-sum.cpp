c957a7a3d6d8c87d84855b7a207693a638d3182c
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-29 01:28:13 +0530

    Time: 12 ms (84.73%), Space: 9.9 MB (96.55%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    int dp[10][256];
-    int score(vector<int>s,vector<int>&m)
-    {
-        int ans=0;
-        int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]==m[i])
-            {
-                ans++;
-            }
-        }
-        return ans;
-    }
-    int helper(int i,int mask,vector<vector<int>>&s,vector<vector<int>>&m)
-    {
-        int n=s.size();
-        if(i==n)
-        {
-            return 0;
-        }
-        if(dp[i][mask]!=-1)
-        {
-            return dp[i][mask];
-        }
-        int ans=0;
-        for(int j=0;j<n;j++)
-        {
-            if((mask&(1<<j))==0)
-            {
-                int n_mask=mask|(1<<j);
-                ans=max(ans,score(s[i],m[j])+helper(i+1,n_mask,s,m));
-            }
-        }
-        return dp[i][mask]=ans;
-    }
+    
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        memset(dp,-1,sizeof(dp));
-        return helper(0,0,students,mentors);
+       int n=students.size();
+        int m=students[0].size();
+        int dp[n+1][(1<<n)];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++)
+        {
+            for(int mask=1;mask<(1<<n);mask++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if((mask&(1<<j)))
+                    {
+                        int score=0;
+                        for(int k=0;k<m;k++)
+                        {
+                          if(students[i-1][k]==mentors[j][k])
+                          {
+                              score++;
+                          }
+                        }
+                        dp[i][mask]=max(dp[i][mask],score+dp[i-1][mask^(1<<j)]);
+                       // cout<<i<<" "<<j<<" "<<mask<<" "<<dp[i][mask]<<endl;
+                    }
+                }
+            }
+        }
+     
+        return dp[n][(1<<n)-1];
     }
 };
