1f02fe7f750d263e30adcf4e4931187b9952d473
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-01-28 12:44:13 +0530

    Time: 91 ms (5.10%), Space: 21 MB (5.40%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    
-    int maxSatisfaction(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+    vector<vector<int>>dp;
+    int helper(vector<int>&arr,int idx,int time)
+    {
         int n=arr.size();
-        if(arr[n-1]<=0)
+        if(idx==n)
         {
             return 0;
         }
-        int ans=0;
-        int curr_sum=0;
-        int suff_sum=0;
-        for(int i=n-1;i>=0;i--)
+        if(dp[idx][time]!=-1)
         {
-            suff_sum+=arr[i];
-            curr_sum+=suff_sum;
-            ans=max(ans,curr_sum);
-            
+            return dp[idx][time];
         }
-        return ans;
+        int taken=arr[idx]*time+helper(arr,idx+1,time+1);
+        int nottaken=helper(arr,idx+1,time);
+        return dp[idx][time]=max(taken,nottaken);
+    }
+    int maxSatisfaction(vector<int>& arr) {
+       sort(arr.begin(),arr.end());
+       int n=arr.size();
+        dp.resize(n+1,vector<int>(n+1,-1));
+        return helper(arr,0,1);
     }
 };
