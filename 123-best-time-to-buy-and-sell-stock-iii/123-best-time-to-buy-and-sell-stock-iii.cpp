5c49890d346272eb517cedfad32fb3846c55a277
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-05-24 02:18:18 +0530

    Time: 454 ms (32.94%), Space: 123.1 MB (34.82%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,27 +3,22 @@ public:
     
     int maxProfit(vector<int>& prices) {
       int n=prices.size();
-        int pre[n];
-        pre[0]=0;
-        int min_buy=prices[0];
-        for(int i=1;i<n;i++)
-        {
-            pre[i]=max(pre[i-1],prices[i]-min_buy);
-            min_buy=min(min_buy,prices[i]);
-        }
-        int suff[n];
-        suff[n-1]=0;
-        int max_sell=prices[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            suff[i]=max(suff[i+1],-prices[i]+max_sell);
-            max_sell=max(max_sell,prices[i]);
-        }
+        int K=2;
+       vector<vector<int>>dp(n,vector<int>(K+1,0));
         int ans=0;
-        for(int i=0;i<n;i++)
+        for(int k=1;k<=K;k++)
         {
-            ans=max(ans,pre[i]+suff[i]);
+            int temp=dp[0][k-1]-prices[0];
+            for(int i=1;i<n;i++)
+            {
+                    dp[i][k]=dp[i-1][k];
+                        dp[i][k]=max(dp[i][k],prices[i]+temp);
+                       temp=max(temp,-prices[i]+dp[i][k-1]);                 
+                    
+             ans=max(ans,dp[i][k]);
+            }
         }
+        
         return ans;
     }
 };
