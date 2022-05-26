ebb80c3f8e477fc08920ea0d0ca2964dddf147b9
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-05-26 15:37:59 +0530

    Time: 345 ms (60.76%), Space: 42.3 MB (46.22%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     int minCost(int maxt, vector<vector<int>>& edges, vector<int>& fee) {
         int n=fee.size();
-        vector<vector<int>>v(n,vector<int>(n,INT_MAX));
+        unordered_map<int,vector<pair<int,int>>>adj;
+        //src vertex time
         for(auto x:edges)
         {
             int a=x[0];
             int b=x[1];
             int t=x[2];
-            v[a][b]=min(v[a][b],t);
-           // v[b][a]=min(v[b][a],t);
-        }
-        unordered_map<int,vector<pair<int,int>>>adj;
-        //src vertex time
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(v[i][j]!=INT_MAX)
-                {
-                    adj[i].push_back({j,v[i][j]});
-                    adj[j].push_back({i,v[i][j]});
-                }
-            }
+            adj[a].push_back({b,t});
+            adj[b].push_back({a,t});
         }
         vector<int>cost(n,INT_MAX);
-        vector<int>time(n,maxt+1);
+        vector<int>time(n,INT_MAX);
         time[0]=0;
         cost[0]=fee[0];
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
@@ -38,24 +26,35 @@ public:
             int ptime=x[1];
             int src=x[2];
             int pcost=x[0];
-           if(src==n-1)
-           {
-               return pcost;
-           }
+            if(src==n-1)
+            {
+                return cost[src];
+            }
             for(auto x:adj[src])
             {
                 int v=x.first;
                 int t=x.second;
-                // maxtime k under trip ho rhi h
-                 int nt = ptime + t, nc = pcost +fee[v];
-                   if(nt<time[v])
-                   {
-                        time[v]=min(time[v],nt);
-                       
-                        pq.push({nc,nt,v});
+                if(ptime+t<=maxt)
+                {
+                    // maxtime k under trip ho rhi h
+                    if(cost[v]>pcost+fee[v])
+                    {
+                        cost[v]=pcost+fee[v];
+                        time[v]=ptime+t;
+                        pq.push({cost[v],time[v],v});
                     }
+                    else if(time[v]>t+ptime)
+                    {
+                        //cost[v]=pcost+fee[v];
+                        time[v]=ptime+t;
+                        pq.push({pcost+fee[v],time[v],v});
+                    } 
+                }
+               
             }
         }
+       
+       
         return -1;
         
     }
