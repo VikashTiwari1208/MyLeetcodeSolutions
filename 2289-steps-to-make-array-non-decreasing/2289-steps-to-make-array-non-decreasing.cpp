44f7d93224b2899eccb437a68f965237d74d1f15
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-05-30 03:12:51 +0530

    Time: 183 ms (75.00%), Space: 72.7 MB (50.00%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     int totalSteps(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,0);
+        //vector<int>dp(n,0);
         stack<pair<int,int>>st;
         st.push({nums[0],0});
         int ans=0;
        
         for(int i=1;i<n;i++)
         {
+            int cnt=0;
             while(st.empty()==false&&st.top().first<=nums[i])
             {
-                dp[i]=max(dp[i],dp[st.top().second]);
+                cnt=max(cnt,st.top().second);
                 st.pop();
             }
             if(st.empty()==false)
             {
-                dp[i]++;
+                cnt++;
             }
             if(st.empty())
             {
-                dp[i]=0;
+                cnt=0;
             }
-            cout<<dp[i]<<" ";
-            ans=max(ans,dp[i]);
-            st.push({nums[i],i});
+            //cout<<dp[i]<<" ";
+            ans=max(ans,cnt);
+            st.push({nums[i],cnt});
         }
-        cout<<endl;
+        //cout<<endl;
         return ans;
     }
 };
