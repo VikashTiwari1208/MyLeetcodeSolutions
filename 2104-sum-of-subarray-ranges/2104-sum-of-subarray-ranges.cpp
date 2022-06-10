f4d5ad498479bee2fc831f93972ca8f0b212e5dc
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-06-10 11:17:16 +0530

    Time: 40 ms (59.27%), Space: 11.5 MB (18.83%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,15 +1,72 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& A) {
-          long long res = 0;
-        for (int i = 0; i < A.size(); i++) {
-            int ma = A[i], mi = A[i];
-            for (int j = i; j < A.size(); j++) {
-                ma = max(ma, A[j]);
-                mi = min(mi, A[j]);
-                res += ma - mi;
+    long long subArrayRanges(vector<int>& v) {
+          long long int ans=0;
+          int n=v.size();
+        vector<int>ple(n,-1),nle(n,n),pge(n,-1),nge(n,n);
+        stack<int>s;
+        s.push(0);
+        for(int i=1;i<n;i++)
+        {
+            while(s.empty()==false&&v[s.top()]>v[i])
+            {
+                s.pop();
             }
+            if(s.empty()==false)
+            {
+                ple[i]=s.top();
+            }
+            s.push(i);
         }
-        return res;
+         while(!s.empty()){s.pop();}
+        s.push(n-1);
+        for(int i=n-2;i>=0;i--)
+        {
+            while(s.empty()==false&&v[s.top()]>=v[i])
+            {
+                s.pop();
+            }
+            if(s.empty()==false)
+            {
+                nle[i]=s.top();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){s.pop();}
+        s.push(0);
+        for(int i=1;i<n;i++)
+        {
+            while(s.empty()==false&&v[s.top()]<v[i])
+            {
+                s.pop();
+            }
+            if(s.empty()==false)
+            {
+                pge[i]=s.top();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){s.pop();}
+        s.push(n-1);
+        for(int i=n-2;i>=0;i--)
+        {
+            while(s.empty()==false&&v[s.top()]<=v[i])
+            {
+                s.pop();
+            }
+            if(s.empty()==false)
+            {
+                nge[i]=s.top();
+            }
+            s.push(i);
+        }
+        for(int i=0;i<n;i++)
+        {
+           long long int g=(i-pge[i])*(nge[i]-i);
+            long long int l=(i-ple[i])*(nle[i]-i);
+          //  cout<<i<<" "<<l<<" "<<g<<endl;
+            ans+=(g-l)*v[i];
+        }
+        return ans;
     }
 };
