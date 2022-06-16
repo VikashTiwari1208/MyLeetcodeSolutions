0fbc131e057734c25e8b3cd96ef1cd052a1b0b6c
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-06-16 14:25:01 +0530

    Time: 718 ms (32.40%), Space: 210.5 MB (17.61%) - LeetHub

---
+++ 2003-smallest-missing-genetic-value-in-each-subtree/2003-smallest-missing-genetic-value-in-each-subtree.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>>adj;
+    vector<int>vis;
+    vector<int>vis2;
+    void dfs(int src,vector<int>&nums)
+    {
+        vis[src]=1;
+        vis2[nums[src]]=1;
+        for(auto x:adj[src])
+        {
+            if(vis[x]==-1)
+            {
+                dfs(x,nums);
+            }
+        }
+    }
+    vector<int> smallestMissingValueSubtree(vector<int>& par, vector<int>& nums) {
+        int n=par.size();
+        vis.resize(n,-1);
+        vis2.resize(1e5+3,-1);
+        for(int i=1;i<n;i++)
+        {
+          adj[par[i]].push_back(i);   
+        }
+        int node=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+            {
+                node=i;
+                break;
+            }
+        }
+        vector<int>ans;
+        ans.resize(n,1);
+       
+        if(node==-1)
+        {
+            return ans;
+        }
+        int curr_mini=2;
+       // cout<<node<<" ";
+        while(node>-1)
+        {
+            dfs(node,nums);
+            while(vis2[curr_mini]==1)
+            {
+                curr_mini++;
+            }
+            ans[node]=curr_mini;
+          //  cout<<node<<" "<<curr_mini<<endl;
+            node=par[node];
+        }
+        return ans;
+    }
+};
