25ae49fabda5e72b18688022229f4b40bcb18412
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-06-29 22:35:27 +0530

    Time: 722 ms (73.61%), Space: 119.9 MB (85.52%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.cpp
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.cpp
@@ -2,43 +2,38 @@ class Solution {
 public:
     vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
         
-        queue<int> pendingTasks;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > freeServers, busyServers;
-        
-        vector<int> ans;
-        int timer = 0;
-        
-        for(int i = 0; i < servers.size(); i++) {
-            freeServers.push({servers[i], i}); 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > free,busy;
+        for(int i=0;i<servers.size();i++)
+        {
+            free.push({servers[i],i});
         }
-        for(int j = 0; j < tasks.size(); j++) {
-            pendingTasks.push(j);
-        }
-        
        
-        while(!pendingTasks.empty()) {
-			
-            while(!busyServers.empty() and busyServers.top().first <= timer) {
-                auto p = busyServers.top();
-                busyServers.pop();
-                freeServers.push({servers[p.second], p.second});
+        int n=tasks.size();
+         vector<int> ans(n);
+        int time=0;
+        for(int i=0;i<n;i++)
+        {
+            time=max(time,i);
+            if(free.empty()==true)
+            {
+               auto  x=busy.top(); // busy se nikalkr push kia;
+                busy.pop();
+                time=x.first; // itna time wait krna pdega busy ko free krne mein
+                free.push({servers[x.second],x.second});
             }
-            
-        
-            while(!freeServers.empty() and !pendingTasks.empty() and pendingTasks.front() <= timer) {
-                int i = pendingTasks.front();
-                auto p = freeServers.top();
-                freeServers.pop();
-                busyServers.push({timer + tasks[i], p.second}); 
-                pendingTasks.pop();
-                ans.push_back(p.second);
+            while(busy.empty()==false&&busy.top().first<=time)
+            {
+                auto x=busy.top();
+                busy.pop();
+                free.push({servers[x.second],x.second});
             }
-            if(freeServers.empty()) {
-                timer = busyServers.top().first;
-                continue;
-            }
-            timer++;
+            auto x=free.top();
+            ans[i]=x.second;
+            free.pop(); // free wale ko assign krdia
+            // add it to running;
+            busy.push({time+tasks[i],x.second});
         }
+        
         
         return ans;   
         
