e098cf944c8c623b821e5bc86a48d90e5222b22e
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-07-24 02:11:35 +0530

    Time: 1179 ms (59.93%), Space: 63.8 MB (40.40%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    void generate_subset_sum(int index,int end,set<int>&s,int sum,vector<int>&nums)
-    {
-        if(index==end)
-        {
-            s.insert(sum);
-           return ;
+    void find(vector<int>&v, int i, int e, int sum, vector<int>&sumv){
+        if(i==e){
+            sumv.push_back(sum);
+            return;
         }
-        generate_subset_sum(index+1,end,s,sum,nums);
-        generate_subset_sum(index+1,end,s,sum+nums[index],nums);
-        
+        find(v,i+1,e,sum+v[i],sumv);
+        find(v,i+1,e,sum,sumv);
     }
+        
+    
     int minAbsDifference(vector<int>& nums, int goal) {
         int n=nums.size();
-        vector<int>s1{0};
-        vector<int>s2{0};
-      for (int i = 0; i < (n / 2); i++) {
-             int v = s1.size();
-            for (int t = 0; t < v; t++) {
-                s1.push_back(s1[t] + nums[i]);
+        
+        //Step 1: Divide nums into 2 subarrays of size n/2 and n-n/2
+        
+        vector<int>A,B;
+        for(int i=0;i<n/2;i++)
+            A.push_back(nums[i]);
+        for(int i=n/2;i<n;i++)
+            B.push_back(nums[i]);
+        
+        //Step 2: Find all possible subset sums of A and B
+        
+        vector<int>sumA,sumB;
+        find(A,0,A.size(),0,sumA);
+        find(B,0,B.size(),0,sumB);
+        
+        sort(sumA.begin(),sumA.end());
+        sort(sumB.begin(),sumB.end());
+        
+        //Step 3: Find combinations from sumA & sumB such that abs(sum-goal) is minimized
+        
+        int ans=INT_MAX;
+        
+        for(int i=0;i<sumA.size();i++){
+            int s=sumA[i];
+            int l=0;
+            int r=sumB.size()-1;
+            while(l<=r){
+                int mid=l+(r-l)/2;
+                int sum=s+sumB[mid];
+                if(sum==goal)
+                    return 0;
+                ans=min(ans,abs(sum-goal));
+                if(sum>goal){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }    
             }
         }
         
-        for (int i = (n / 2); i < n; i++) {
-             int v = s2.size();
-            for (int t = 0; t < v; t++) {
-                s2.push_back(s2[t] + nums[i]);
-            }
-        }
-         int ans=1e9;
-         set<int> s(s2.begin(), s2.end());
-        for(auto x:s1)
-        {
-            int val=goal-x;
-            auto itr=s.lower_bound(val);
-            if(itr!=s.end())
-            {
-                ans=min(ans,abs(goal-(x+*itr)));
-                if(ans==0)
-                {
-                    return 0;
-                }
-              
-            }
-            if(itr!=s.begin())
-            {
-                auto itr2=itr;
-                itr2--;
-                if(itr2!=s.end())
-                {
-                     ans=min(ans,abs(goal-(x+*itr2)));
-                }
-            }
-        }
         return ans;
     }
 };
