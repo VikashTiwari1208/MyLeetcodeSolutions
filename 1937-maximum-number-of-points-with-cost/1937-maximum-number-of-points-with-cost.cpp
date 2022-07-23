6432d522a4d327d6714d272927a8de3eb5505d22
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-07-23 22:14:49 +0530

    Time: 307 ms (91.26%), Space: 82.4 MB (97.78%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -4,38 +4,28 @@ public:
     long long maxPoints(vector<vector<int>>& point) {
         int n=point.size();
         int m=point[0].size();
-        vector<vector<long long int>>dp(2,vector<long long int>(m,-1e16));
+        vector<long long int>dp(m,0);
         long long int ans=-1e16;
-        for(int i=0;i<m;i++)
+      
+        for(int i=0;i<n;i++)
         {
-            dp[0][i]=point[0][i];
-            ans=max(ans,dp[0][i]);
-        }
-        
-        for(int i=1;i<n;i++)
-        {
-            vector<long long int>left(m+1,-1e16),right(m+1,-1e16);
-            long long int maxil=-1e16,maxir=-1e16;
             for(int j=0;j<m;j++)
             {
-                left[j+1]=max(dp[0][j]+j,left[j]);
-               // maxil=max(maxil,left[j]);
+               dp[j]+=point[i][j];
             }
-            for(int j=m-1;j>=0;j--)
-            {
-                right[j]=max(dp[0][j]-j,right[j+1]);
-               // maxir=max(maxir,right[j]);
+            for (int j = 1; j <m; j++) {
+                dp[j] = max(dp[j], dp[j - 1] - 1);
             }
-            for(int j=0;j<m;j++)
-            {
-                dp[1][j]=max({dp[1][j],point[i][j]+left[j+1]-j,point[i][j]+right[j+1]+j});
+            ans=dp[m-1];
+             for (int j =m-2; j>=0; j--) {
+                dp[j] = max(dp[j], dp[j + 1] - 1);
                 if(i==n-1)
-                {
-                    ans=max(ans,dp[1][j]);
-                }
+            {
+                ans=max(dp[j],ans);
             }
-            swap(dp[0],dp[1]);
-            dp[1].clear();
+             }
+            
+            
         }
         return ans;
     }
