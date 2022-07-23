fa0798744a01e14ead02768b343ea0e82fbc858d
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-07-23 21:35:45 +0530

    Time: 473 ms (43.37%), Space: 129.7 MB (5.62%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+   
+    long long maxPoints(vector<vector<int>>& point) {
+        int n=point.size();
+        int m=point[0].size();
+        vector<vector<long long int>>dp(n,vector<long long int>(m,-1e16));
+        long long int ans=-1e16;
+        for(int i=0;i<m;i++)
+        {
+            dp[0][i]=point[0][i];
+            ans=max(ans,dp[0][i]);
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            vector<long long int>left(m+1,-1e16),right(m+1,-1e16);
+            long long int maxil=-1e16,maxir=-1e16;
+            for(int j=0;j<m;j++)
+            {
+                left[j+1]=max(dp[i-1][j]+j,left[j]);
+               // maxil=max(maxil,left[j]);
+            }
+            for(int j=m-1;j>=0;j--)
+            {
+                right[j]=max(dp[i-1][j]-j,right[j+1]);
+               // maxir=max(maxir,right[j]);
+            }
+            for(int j=0;j<m;j++)
+            {
+                dp[i][j]=max({dp[i][j],point[i][j]+left[j+1]-j,point[i][j]+right[j+1]+j});
+                if(i==n-1)
+                {
+                    ans=max(ans,dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
