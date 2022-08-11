f05a5c7e07bd630b254959875e8338fae267d7ba
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-08-12 03:51:27 +0530

    Time: 452 ms (38.21%), Space: 8 MB (88.62%) - LeetHub

--- 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    int dp[100002][7][7];
     int mod=1e9+7;
+    /*int dp[100002][7][7];
+    
     int helper(int idx,int last1,int last2,int &n)
     {
         if(idx>=n)
@@ -22,9 +23,34 @@ public:
             }
         }
         return dp[idx][last1][last2]=ans;
-    }
+    }*/
     int distinctSequences(int n) {
-        memset(dp,-1,sizeof(dp));
-        return helper(0,0,0,n);
+        int dp[n+1][7][7];
+        for(int i=0;i<7;i++)
+        {
+            for(int j=0;j<7;j++)
+            {
+                dp[n][i][j]=1;
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+               for(int prev1=0;prev1<7;prev1++)
+              {
+                for(int prev2=0;prev2<7;prev2++)
+                {
+                    int ans=0;
+                    for(int curr=1;curr<=6;curr++)
+                  {
+                   if(prev1!=curr&&prev2!=curr&&(__gcd(prev1,curr)==1||prev1==0))
+                   {
+                      ans=(ans%mod+dp[i+1][curr][prev1]%mod)%mod;
+                   }
+                }
+              dp[i][prev1][prev2]=ans;      
+             }   
+            }
+        }
+        return dp[0][0][0];
     }
 };
