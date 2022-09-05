518bd9e2c14514564ae96960802051f958af962f
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-09-05 14:42:10 +0530

    Time: 12 ms (45.60%), Space: 7 MB (46.32%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
        
-        int m1[256]={0}, m2[256]={0};
-        int count = 1;
-        for(int i=0; i<s.length(); i++) {
-            if(m1[s[i]]!=m2[t[i]]) return false;
-            if(m1[s[i]]==0) {
-                m1[s[i]]=count;
-                m2[t[i]]=count;
-                count++;
-            }
+       int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
         return true;
     }
