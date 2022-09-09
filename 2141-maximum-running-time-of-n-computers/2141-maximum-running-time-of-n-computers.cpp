4fad8988e1c531b13970fe80796d60b79f86f99d
Vikas Tiwari <58940513+VikashTiwari1208@users.noreply.github.com>
2022-09-09 23:41:55 +0530

    Time: 372 ms (19.02%), Space: 55.8 MB (88.95%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>&v) {
+        long long int ans=0;
+       long long int l=0;
+        long long int r=0;
+        for(auto x:v)
+        {
+            r+=x;
+        }
+        r=r/n;
+        sort(v.begin(),v.end());
+        while(l<=r)
+        {
+            long long int mid=l+(r-l)/2;
+            long long int sum=0;
+            int cnt=0;
+            for(auto x:v)
+            {
+              sum+=x;
+                if(sum>=mid)
+                {
+                    sum-=mid;
+                    cnt++;
+                }
+            }
+                        
+            if(cnt>=n)
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
